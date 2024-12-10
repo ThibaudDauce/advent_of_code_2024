009c7176d358d4ef205b60ad6413e28ff50644bf
Thibaud Dauce <git@hadibut.fr>
2024-12-10 10:54:14 +0100

    Day 06 - Part 2

--- src/06.rs
+++ src/06.rs
@@ -19,6 +19,7 @@ fn part1() {
         }
     }
 
+    let initial_position = position.unwrap();
     let mut position = position.unwrap();
 
     while let Some(is_bloc) = map.get(&position) {
@@ -41,6 +42,40 @@ fn part1() {
     }
 
     println!("Part 1 is {}", visited.len());
+
+    visited.remove(&initial_position);
+    let mut sum = 0;
+    for new_bloc in visited {
+        position = initial_position;
+        direction = (0, -1);
+
+        let mut loop_info = HashSet::new();
+
+        while let Some(is_bloc) = map.get(&position) {
+            if *is_bloc || new_bloc == position {
+                position.0 -= direction.0;
+                position.1 -= direction.1;
+
+                direction = match direction {
+                    (0, -1) => (1, 0),
+                    (1, 0) => (0, 1),
+                    (0, 1) => (-1, 0),
+                    (-1, 0) => (0, -1),
+                    _ => panic!(),
+                }
+            }
+
+            if loop_info.contains(&(position, direction)) {
+                sum += 1;
+                break;
+            }
+            loop_info.insert((position, direction));
+            position.0 += direction.0;
+            position.1 += direction.1;
+        }
+    }
+
+    println!("Part 2 is {sum}");
 }
 
 fn example() -> &'static str {
